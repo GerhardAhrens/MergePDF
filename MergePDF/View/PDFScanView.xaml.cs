4fc334587a081b7ffdaf0271ff43e6a336e9724f
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2026-07-24 19:32:50 +0200

    *

--- MergePDF/View/PDFScanView.xaml.cs
+++ MergePDF/View/PDFScanView.xaml.cs
@@ -15,12 +15,14 @@
 
 namespace MergePDF.View
 {
-    using System.Collections.ObjectModel;
     using System.ComponentModel;
     using System.Diagnostics;
+    using System.IO;
+    using System.Runtime.InteropServices;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Data;
+    using System.Windows.Media.Imaging;
 
     using MergePDF.Core;
 
@@ -41,6 +43,7 @@ namespace MergePDF.View
 
             this.GoBackCommand = new CommandBase(commandParam => this.OnGoBack(commandParam), () => true);
             this.SacnPDFCommand = new CommandBase(commandParam => this.OnSanPDF(commandParam), () => true);
+            this.SaveFileCommand = new CommandBase(commandParam => this.OnSaveFile(commandParam), () => true);
 
             this.DataContext = this;
         }
@@ -48,6 +51,7 @@ namespace MergePDF.View
         #region Properties
         public CommandBase GoBackCommand { get; private set; }
         public CommandBase SacnPDFCommand { get; private set; }
+        public CommandBase SaveFileCommand { get; private set; }
 
         public ICollectionView ScannerSource
         {
@@ -61,6 +65,18 @@ namespace MergePDF.View
             set => base.SetValue(value);
         }
 
+        public BitmapImage ScannedImage
+        {
+            get => base.GetValue<BitmapImage>();
+            set => base.SetValue(value);
+        }
+
+        public string FileSuffix
+        {
+            get => base.GetValue<string>();
+            set => base.SetValue(value);
+        }
+
         private ChangeViewEventArgs CurrentCtorArgs { get; set; }
         private MessageBase Message { get; } = new MessageBase();
         #endregion Properties
@@ -71,15 +87,11 @@ namespace MergePDF.View
         {
             List<ScannerInfo> scanners = new();
             DeviceManager manager = new();
+
             foreach (DeviceInfo deviceInfo in manager.DeviceInfos)
             {
                 if (deviceInfo.Type == WiaDeviceType.ScannerDeviceType)
                 {
-                    foreach (Property prop in deviceInfo.Properties)
-                    {
-                        Debug.WriteLine($"{prop.Name}: {prop.get_Value()}");
-                    }
-
                     ScannerInfo si = new();
                     si.Name = deviceInfo.Properties["Name"].get_Value().ToString();
                     si.Description = deviceInfo.Properties["Description"].get_Value().ToString();
@@ -89,6 +101,15 @@ namespace MergePDF.View
                     si.DeviceInfo = deviceInfo;
                     scanners.Add(si);
                 }
+            }
+
+            if (scanners.Any() == false)
+            {
+                ScannerInfo si = new();
+                si.Name = "Kein Scanner";
+                si.Description = "oder Offline";
+                scanners.Add(si);
+                this.BtnScanPDF.IsEnabled = false;
             }
 
             this.ScannerSource = CollectionViewSource.GetDefaultView(scanners);
@@ -126,18 +147,100 @@ namespace MergePDF.View
                 {
                     try
                     {
-                        Device scanner = SelectedScanner.DeviceInfo.Connect();
-                        Item item = scanner.Items[1];
+                        (bool, string) isOk = IsScannerOnline(this.SelectedScanner.DeviceInfo);
+                        if (isOk.Item1 == true)
+                        {
+                            Device scanner = this.SelectedScanner.DeviceInfo.Connect();
+                            Item item = scanner.Items[1];
+
+                            string format = "{B96B3CAF-0728-11D3-9D7B-0000F81EF32E}"; // PNG
+                            ImageFile image = (ImageFile)new CommonDialog().ShowTransfer(item, format, false);
+
+                            byte[] bytes = (byte[])image.FileData.get_BinaryData();
+
+                            using MemoryStream ms = new(bytes);
+
+                            BitmapImage bitmap = new();
+                            bitmap.BeginInit();
+                            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                            bitmap.StreamSource = ms;
+                            bitmap.EndInit();
+                            bitmap.Freeze();
+
+                            this.ScannedImage = bitmap;
+                        }
+                        else
+                        {
+                            this.Message.Error("Dokument scannen", isOk.Item2);
+                        }
                     }
                     catch (Exception ex)
                     {
-                        this.Message.Error("Dokument Scannen", $"Es ist ein Problem mit einem Scannner aufgetreten \n{ex.Message}");
+                        this.Message.Error("Dokument Scannen", $"Es ist ein Problem mit dem Scannner aufgetreten \n{ex.Message}");
                     }
+                }
+            }
+        }
+
+        private void OnSaveFile(object commandParam)
+        {
+            if (commandParam != null && commandParam is CommandButtons button)
+            {
+                if (button == CommandButtons.SavePDF)
+                {
+
                 }
             }
         }
 
         #endregion Command Events
 
+        public (bool,string) IsScannerOnline(DeviceInfo deviceInfo)
+        {
+            bool result = false;
+            string errorText = string.Empty;
+
+            try
+            {
+                Device device = deviceInfo.Connect();
+                result = device != null;
+                return (result, string.Empty);
+            }
+            catch (COMException ex) when ((uint)ex.ErrorCode == 0x80210015)
+            {
+                errorText = "Offline";
+                return (result, errorText);
+            }
+            catch (COMException ex) when ((uint)ex.ErrorCode == 0x80210006)
+            {
+                errorText = "Belegt";
+                return (result, errorText);
+            }
+            catch (COMException ex) when ((uint)ex.ErrorCode == 0x80210064)
+            {
+                errorText = "Papierstau";
+                return (result, errorText);
+            }
+            catch (COMException ex) when ((uint)ex.ErrorCode == 0x8021000C)
+            {
+                errorText = "Gerät nicht verfügbar";
+                return (result, errorText);
+            }
+            catch (COMException ex) when ((uint)ex.ErrorCode == 0x80210016)
+            {
+                errorText = "Papierfach leer (bei ADF, treiberabhängig)";
+                return (result, errorText);
+            }
+            catch (COMException ex) when ((uint)ex.ErrorCode == 0x80210003)
+            {
+                errorText = "Allgemeiner WIA-Fehler";
+                return (result, errorText);
+            }
+            catch
+            {
+                errorText = "Unbekanter Fehler";
+                return (result, errorText);
+            }
+        }
     }
 }
